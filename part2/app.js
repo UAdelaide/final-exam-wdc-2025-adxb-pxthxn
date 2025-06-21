07c7e6368ab5ce69d7e1b15288c54dcae3b64c95
Adib <adibpathan280@gmail.com>
2025-06-21 05:25:25 +0000

    [automatic commit] 2025-06-21 05:25:25 UTC 180.150.112.57

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(express.urlencoded({ extended: true })); // To parse form POSTs
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Routes
+app.use(session({
+  secret: 'superdogsecret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Route imports
+const indexRoutes = require('./routes/index'); // <-- Your login + dashboard routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+// Route mounting
+app.use('/', indexRoutes); 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
 module.exports = app;
